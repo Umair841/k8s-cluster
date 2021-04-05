5106c34bd50bfb5b18bab1626e36f1c31af59292
Umair841 <77967477+Umair841@users.noreply.github.com>
2021-04-05 19:29:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return ' deployment ok k8s cluster '
+    return ' deployment ok k8s cluster..!!.. cicdpipeline '
 
 app.run(host='0.0.0.0', port=5000)
